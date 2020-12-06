74e19156efdc32b965a9a066dbcdb043914b69f8
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-12-06 17:48:01 +0100

    part 2 added correctly

--- year_2020/ex6.py
+++ year_2020/ex6.py
@@ -3,19 +3,21 @@ with open("data.txt") as f:
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
 
-set_answered_questions = set()
+set_all_yes_questions = None
 
 nb_yes_answer = 0
 for line in content:
     if line == "":
-        nb_yes_answer += len(set_answered_questions)
-        set_answered_questions = set()
+        nb_yes_answer += len(set_all_yes_questions)
+        set_all_yes_questions = None
         continue
-    for char in line:
-        set_answered_questions.add(char)
+    if set_all_yes_questions is None:
+        set_all_yes_questions = set(line)
+    else:
+        set_all_yes_questions = set_all_yes_questions.intersection(set(line))
+
 
 # for the last iteration not appended
-nb_yes_answer += len(set_answered_questions)
+nb_yes_answer += len(set_all_yes_questions)
 
-# 6518 too low
 print(nb_yes_answer)
