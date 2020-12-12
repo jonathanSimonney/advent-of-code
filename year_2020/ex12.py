975beacb0ffaa8573e0632aa69706e9ac4c45e0b
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-12-12 21:36:54 +0100

    part2 ex12 done

--- year_2020/ex12.py
+++ year_2020/ex12.py
@@ -1,12 +1,26 @@
-def move_ship_in_direction(ship_info_param, direction_to_move, value_to_move):
+def move_element_in_direction(element_info_param, direction_to_move, value_to_move):
     if direction_to_move == 'N':
-        ship_info_param["y_pos"] += value_to_move
+        element_info_param["y_pos"] += value_to_move
     elif direction_to_move == 'S':
-        ship_info_param["y_pos"] -= value_to_move
+        element_info_param["y_pos"] -= value_to_move
     elif direction_to_move == 'E':
-        ship_info_param["x_pos"] += value_to_move
+        element_info_param["x_pos"] += value_to_move
     elif direction_to_move == 'W':
-        ship_info_param["x_pos"] -= value_to_move
+        element_info_param["x_pos"] -= value_to_move
+
+
+def move_waypoint_right(waypoint_pos):
+    base_x = waypoint_pos["x_pos"]
+    base_y = waypoint_pos["y_pos"]
+    waypoint_pos["x_pos"] = base_y
+    waypoint_pos["y_pos"] = -base_x
+
+
+def move_waypoint_left(waypoint_pos):
+    base_x = waypoint_pos["x_pos"]
+    base_y = waypoint_pos["y_pos"]
+    waypoint_pos["x_pos"] = -base_y
+    waypoint_pos["y_pos"] = base_x
 
 
 def move_ship_with_instruction(ship_info_param, instruction):
@@ -14,22 +28,25 @@ def move_ship_with_instruction(ship_info_param, instruction):
     instruction_value = int(instruction[1:])
 
     if instruction_action == 'L':
-        ship_info_param["current_facing_index"] = (ship_info_param["current_facing_index"] - int(instruction_value / 90)) % 4
+        nb_times_to_move_waypoint = int(instruction_value / 90)
+        for i in range(nb_times_to_move_waypoint):
+            move_waypoint_left(ship_info_param["waypoint_infos"])
     elif instruction_action == 'R':
-        ship_info_param["current_facing_index"] = (int(instruction_value / 90) + ship_info_param["current_facing_index"]) % 4
+        nb_times_to_move_waypoint = int(instruction_value / 90)
+        for i in range(nb_times_to_move_waypoint):
+            move_waypoint_right(ship_info_param["waypoint_infos"])
     elif instruction_action == 'F':
-        move_ship_in_direction(ship_info_param, tuple_directions[ship_info_param["current_facing_index"]], instruction_value)
+        ship_info_param["x_pos"] += instruction_value * ship_info_param["waypoint_infos"]["x_pos"]
+        ship_info_param["y_pos"] += instruction_value * ship_info_param["waypoint_infos"]["y_pos"]
     else:
-        move_ship_in_direction(ship_info_param, instruction_action, instruction_value)
+        move_element_in_direction(ship_info_param["waypoint_infos"], instruction_action, instruction_value)
 
 
 with open("data.txt") as f:
     list_instruction = [x.strip() for x in f.readlines()]
 # you may also want to remove whitespace characters like `\n` at the end of each line
 
-tuple_directions = ('E', 'S', 'W', 'N')
-
-ship_info = {"x_pos": 0, "y_pos": 0, "current_facing_index": 0}
+ship_info = {"x_pos": 0, "y_pos": 0, "waypoint_infos": {"x_pos": 10, "y_pos": 1}}
 
 for instruction_str in list_instruction:
     move_ship_with_instruction(ship_info, instruction_str)
