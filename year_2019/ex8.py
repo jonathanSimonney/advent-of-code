6fd3d1790f5694f19cbce7e4c06fedbf8226cecc
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-12-13 14:40:56 +0100

    ex8 finished

--- year_2019/ex8.py
+++ year_2019/ex8.py
@@ -21,6 +21,25 @@ def count_str_in_layer(layer_to_search, str_to_count):
         acc += row.count(str_to_count)
     return acc
 
+
+def recursive_compute_resulting_image(array_layers, current_image):
+    for row_idx, row in enumerate(current_image):
+        current_row = list(row)
+        for col_idx, col_char in enumerate(current_row):
+            if col_char == '2':
+                current_row[col_idx] = array_layers[0][row_idx][col_idx]
+        current_image[row_idx] = "".join(current_row)
+
+    if len(array_layers) == 1:
+        return current_image
+    return recursive_compute_resulting_image(array_layers[1:], current_image)
+
+
+def ascii_print_image(image_to_print):
+    for row in image_to_print:
+        print(row.replace('0', '.').replace('1', '#'))
+
+
 with open("data.txt") as f:
     content = f.readlines()[0]
 
@@ -28,8 +47,6 @@ width = 25
 height = 6
 
 parsed_image = parse_image_in_layers(content, width, height)
-
-print(parsed_image)
 
 min_nb_0_found = None
 layer_with_min_nb_0 = None
@@ -40,3 +57,8 @@ for layer in parsed_image:
         min_nb_0_found = candidate_min_nb_0_found
 
 print(count_str_in_layer(layer_with_min_nb_0, '1') * count_str_in_layer(layer_with_min_nb_0, '2'))
+
+single_layer_image = recursive_compute_resulting_image(parsed_image, parsed_image[0])
+print(single_layer_image)
+# CFCUS isn't right
+ascii_print_image(single_layer_image)
