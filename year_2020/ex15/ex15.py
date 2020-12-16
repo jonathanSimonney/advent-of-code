63fc860d7bdf8cbeb27b7fc86415d07d8285a2b5
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-12-16 23:56:04 +0100

    ex15 easier to understand hopefully

--- year_2020/ex15/ex15.py
+++ year_2020/ex15/ex15.py
@@ -1,23 +1,24 @@
 import time
 import array
 
-# list_starting_nums = [0, 3]
-list_starting_nums = [6, 19, 0, 5, 7, 13]
+# list_starting_nums = [0, 3, 6]
+list_starting_nums = [6, 19, 0, 5, 7, 13, 1]
 
 list_spoken_nbers = array.array('i', (-1,)*30000000)
 tLess1 = time.time()
 
-for idx, num in enumerate(list_starting_nums):
+for idx, num in enumerate(list_starting_nums[:-1]):
     list_spoken_nbers[num] = idx
 
 # nb_spoken_nums = -1
-last_spoken_num = 1
+last_spoken_num = list_starting_nums[-1]
 
 max_treshold = 30000000
 
 t0 = time.time()
 print(t0 - tLess1)
-for nb_spoken_nums in range(len(list_starting_nums), max_treshold - 1):
+range_to_iterate_on = range(len(list_starting_nums) - 1, max_treshold - 1)
+for nb_spoken_nums in range_to_iterate_on:
 
     if list_spoken_nbers[last_spoken_num] == -1:
         current_spoken_num = 0
@@ -31,5 +32,5 @@ for nb_spoken_nums in range(len(list_starting_nums), max_treshold - 1):
 
 print(last_spoken_num)
 t1 = time.time()
-# less than 10 seconds
+# approx 10 seconds
 print(t1 - t0)
