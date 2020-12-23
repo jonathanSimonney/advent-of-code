4288e0ee76739d7f6aeac32df4790965f61ec6f5
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2020-12-23 16:51:27 +0100

    linked list are fun

--- year_2020/ex23.py
+++ year_2020/ex23.py
@@ -1,57 +1,89 @@
-def do_one_move(list_cups_to_apply_move):
-    removed_list_cups = []
-    next_list_cups = []
-    current_cup_number = list_cups_to_apply_move[current_cup_index]
+def do_one_move(linked_dict_to_apply_move):
+    removed_set_cups = set()
 
     # remove next 3 cups and keep them
-    first_removed_cup_label = list_cups_to_apply_move[(current_cup_index + 1) % 9]
-    second_removed_cup_label = list_cups_to_apply_move[(current_cup_index + 2) % 9]
-    third_removed_cup_label = list_cups_to_apply_move[(current_cup_index + 3) % 9]
-    # print(f"pick up: {first_removed_cup_label}, {second_removed_cup_label}, {third_removed_cup_label}")
+    first_removed_cup_label = linked_dict_to_apply_move[current_cup_label]
+    second_removed_cup_label = linked_dict_to_apply_move[first_removed_cup_label]
+    third_removed_cup_label = linked_dict_to_apply_move[second_removed_cup_label]
 
-    removed_list_cups.append(first_removed_cup_label)
-    removed_list_cups.append(second_removed_cup_label)
-    removed_list_cups.append(third_removed_cup_label)
+    # chain of cups now goes from current to fourth cup
+    linked_dict_to_apply_move[current_cup_label] = linked_dict_to_apply_move[third_removed_cup_label]
 
-    list_cups_to_apply_move.remove(first_removed_cup_label)
-    list_cups_to_apply_move.remove(second_removed_cup_label)
-    list_cups_to_apply_move.remove(third_removed_cup_label)
+    removed_set_cups.add(first_removed_cup_label)
+    removed_set_cups.add(second_removed_cup_label)
+    removed_set_cups.add(third_removed_cup_label)
 
     # find a proper destination_number
-    destination_number = current_cup_number - 1
+    destination_number = current_cup_label - 1
 
-    while destination_number not in list_cups_to_apply_move:
+    while destination_number in removed_set_cups or destination_number < 1:
         if destination_number < 1:
-            destination_number = 10
+            destination_number = 1000001
+            # destination_number = 10
         destination_number -= 1
 
-    # print(f"destination: {destination_number}")
 
     # append the elems removed right after the destination cup
-    for cup_label in list_cups_to_apply_move:
-        next_list_cups.append(cup_label)
-        if cup_label == destination_number:
-            next_list_cups.extend(removed_list_cups)
+    linked_dict_to_apply_move[third_removed_cup_label] = linked_dict_to_apply_move[destination_number]
+    linked_dict_to_apply_move[destination_number] = first_removed_cup_label
 
-    # find the next cup index
-    for idx_cup, cup_label in enumerate(next_list_cups):
-        if cup_label == current_cup_number:
-            next_current_cup_index = (idx_cup + 1) % 9
-
-    return {"list_cups": next_list_cups, "index_current": next_current_cup_index}
+    return linked_dict_to_apply_move[current_cup_label]
 
 
-list_cups = [1, 9, 8, 7, 5, 3, 4, 6, 2]
-# list_cups = [3, 8, 9, 1, 2, 5, 4, 6, 7]
-current_cup_index = 0
+chain_cups = {
+    1: 9,
+    9: 8,
+    8: 7,
+    7: 5,
+    5: 3,
+    3: 4,
+    4: 6,
+    6: 2,
+    2: 1
+}
 
-for i in range(100):
-    # current_cup_label = list_cups[current_cup_index]
+first_cup_label = 1
+last_cup_label = 2
+
+# chain_cups = {
+#     3: 8,
+#     8: 9,
+#     9: 1,
+#     1: 2,
+#     2: 5,
+#     5: 4,
+#     4: 6,
+#     6: 7,
+#     7: 3
+# }
+#
+# first_cup_label = 3
+# last_cup_label = 7
+
+label_to_add = 10
+while len(chain_cups) != 1000000:
+    chain_cups[last_cup_label] = label_to_add
+
+    last_cup_label = label_to_add
+    label_to_add += 1
+
+chain_cups[last_cup_label - 1] = first_cup_label
+
+# current_cup_label = 3
+current_cup_label = 1
+
+# for i in range(100):
+for i in range(10000000):
     # print(f"-- move {i + 1} --")
-    # print(f"cups: {['(' + str(x) + ')' if x == current_cup_label else str(x) for x in list_cups]}")
-    result_move = do_one_move(list_cups)
-    list_cups = result_move["list_cups"]
-    current_cup_index = result_move["index_current"]
+    # print(chain_cups)
+    current_cup_label = do_one_move(chain_cups)
+    # if i % 1000 == 0:
+    #     print(f"iteration number {i} done")
 
 #42789356 too low
-print(list_cups, current_cup_index)
+# print(chain_cups, current_cup_label)
+
+# 6212592000 too low
+# 695032060556 too high
+print(chain_cups[1], chain_cups[chain_cups[1]])
+print(chain_cups[1] * chain_cups[chain_cups[1]])
