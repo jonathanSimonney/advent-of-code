2ecbf4adb5d1319371be87033509aa576969bf94
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-02-14 17:16:00 +0100

    ex2 2018 completed

--- year_2018/ex2.py
+++ year_2018/ex2.py
@@ -1,13 +1,34 @@
 import collections
 
 
+# copypasted from https://stackoverflow.com/a/25216604/7059810
+def match(s1, s2):
+    ok = False
+
+    for c1, c2 in zip(s1, s2):
+        if c1 != c2:
+            if ok:
+                return False
+            else:
+                ok = True
+
+    return ok
+
+
 with open("data.txt") as f:
     content = f.readlines()
 
 nb_2_letters = 0
 nb_3_letters = 0
 
-for line in content:
+should_break = False
+for idx, line in enumerate(content):
+    if not should_break:
+        for compared_line in content[idx + 1:]:
+            if match(line, compared_line):
+                print(f"match found! between {line} and {compared_line}")
+                should_break = True
+                break
     counter_line = collections.Counter(line)
 
     should_update_2_letters = False
