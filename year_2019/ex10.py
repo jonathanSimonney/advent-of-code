33583f6040a77e48608a87dcf0ad64f2dceeb96c
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-02-28 18:40:12 +0100

    wip for ex 10

--- year_2019/ex10.py
+++ year_2019/ex10.py
@@ -1,3 +1,20 @@
+def concat_in_order(point_1, point_2):
+    return f"x{point_1['x']}y{point_1['y']} x{point_2['x']}y{point_2['y']}"
+
+
+def compute_key_str_los(point_1, point_2):
+    if point_1["x"] < point_2["x"]:
+        return concat_in_order(point_1, point_2)
+    if point_1["x"] > point_2["x"]:
+        return concat_in_order(point_2, point_1)
+    if point_1["y"] < point_2["y"]:
+        return concat_in_order(point_1, point_2)
+    if point_1["y"] > point_2["y"]:
+        return concat_in_order(point_2, point_1)
+    raise AttributeError("2 params have the same value")
+
+
+
 with open("data.txt") as f:
     content = f.readlines()
 
@@ -6,7 +23,7 @@ print(content)
 for x, line in enumerate(content):
     for y, char in enumerate(line):
         if char == "#":
-            list_asteroids_loc.append((x, y))
+            list_asteroids_loc.append({"x": x, "y": y})
 
 print(list_asteroids_loc, len(list_asteroids_loc))
 
