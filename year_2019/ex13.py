e175c785d9b1e87b3343d14b8a5df202b57732a0
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-06-04 09:26:58 +0200

    ex13 part 2 working

--- year_2019/ex13.py
+++ year_2019/ex13.py
@@ -15,13 +15,21 @@ def compute_key_with_coords(tile_dict):
     return f"x{tile_dict['x']}y{tile_dict['y']}"
 
 
-def compute_x_impact_coords(current_ball_pos, previous_ball_pos, dict_tiles_current, y_pos_paddle):
+def compute_x_impact_coords(current_ball_pos, previous_ball_pos, dict_tiles_current, pos_paddle):
     global max_x
     global max_y
 
-    current_ball_x_dir = 1 if current_ball_pos['x'] > previous_ball_pos['x'] else -1
-    next_step_ball_x = current_ball_pos['x'] + current_ball_x_dir
-    return -12
+    is_ball_moving_down = current_ball_pos['y'] > previous_ball_pos['y']
+    is_ball_moving_right = current_ball_pos['x'] > previous_ball_pos['x']
+    multiplicator_coeff = 1 if is_ball_moving_right else -1
+
+    print(is_ball_moving_down, is_ball_moving_right)
+
+    if is_ball_moving_down:
+        nb_moves_left = pos_paddle['y'] - current_ball_pos['y']
+        return current_ball_pos['x'] + nb_moves_left * multiplicator_coeff
+    else:
+        return current_ball_pos['x']
 
 
 def get_input():
@@ -31,27 +39,35 @@ def get_input():
     global paddle_pos
     global ball_pos
     global ball_pos_n1
+    global current_impact_point_x
 
-    paint_tiles(dict_tiles, max_x, max_y)
 
     if ball_pos_n1 == {}:
+        paint_tiles(dict_tiles, max_x, max_y)
         return 0
 
     print(ball_pos)
     print(ball_pos_n1)
     print(paddle_pos)
 
-
-    expected_impact_ball_x = compute_x_impact_coords(ball_pos, ball_pos_n1, dict_tiles, paddle_pos['y'])
+    expected_impact_ball_x = compute_x_impact_coords(ball_pos, ball_pos_n1, dict_tiles, paddle_pos)
+    current_impact_point_x = expected_impact_ball_x
+    print(expected_impact_ball_x, paddle_pos)
+    paint_tiles(dict_tiles, max_x, max_y)
 
     if expected_impact_ball_x == paddle_pos['x']:
         print('ball going ALREADY on paddle')
+        if paddle_pos['y'] == ball_pos['y'] + 1 and ball_pos['y'] > ball_pos_n1['y']:
+            return -1 if ball_pos['x'] > ball_pos_n1['x'] else 1
+        # return int(input("override 0 ?"))
         return 0
     elif expected_impact_ball_x > paddle_pos['x']:
         print('ball going on right of paddle, let\'s go right')
+        # return int(input("override 1 ?"))
         return 1
     else:
         print('ball going on left of paddle, let\'s go left')
+        # return int(input("override -1 ?"))
         return -1
 
 
@@ -95,6 +111,9 @@ def handle_output(value):
 
 
 def paint_tiles(dict_tiles_to_paint, max_x_to_paint, max_y_to_paint):
+    global current_impact_point_x
+    global paddle_pos
+
     dict_symbols = {
         DrawnObjects.EMPTY: ' ',
         DrawnObjects.WALL: '.',
@@ -106,7 +125,10 @@ def paint_tiles(dict_tiles_to_paint, max_x_to_paint, max_y_to_paint):
     str_to_print = ''
     for y in range(max_y_to_paint + 1):
         for x in range(max_x_to_paint + 1):
-            str_to_print += dict_symbols[dict_tiles_to_paint[compute_key_with_coords({'x': x, 'y': y})]]
+            if y == paddle_pos['y'] and x == current_impact_point_x:
+                str_to_print += 'I'
+            else:
+                str_to_print += dict_symbols[dict_tiles_to_paint[compute_key_with_coords({'x': x, 'y': y})]]
         str_to_print += '\n'
 
     print(str_to_print)
@@ -127,7 +149,7 @@ ball_pos = {}
 paddle_pos = {}
 max_x = 0
 max_y = 0
-
+current_impact_point_x = -1
 
 intcode_computer = IntcodeComputer(content)
 
