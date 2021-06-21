c93645737135ca73afe68067e8fd19a04fe05589
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-06-21 07:53:10 +0200

    finally managed part 2 properly

--- year_2019/ex14.py
+++ year_2019/ex14.py
@@ -83,6 +83,17 @@ def run_system_until_no_excess_product_remains(dict_reactions_filled):
                 revert_production_chemical(reactive_name, reactive_amount, dict_reactions_filled)
 
 
+def produce_fuel_with_shortcut(nb_ore_input, nb_fuel_for_ore):
+    global dict_remaining_chemicals
+    global dict_needed_chemicals
+    global dict_chemical_producted
+    nb_time_can_rerun = math.floor(nb_ore_input / nb_fuel_for_ore)
+
+    for chemical_name, chemical_amount in dict_chemical_producted.items():
+        dict_remaining_chemicals[chemical_name] += nb_time_can_rerun * chemical_amount
+    dict_needed_chemicals['ORE'] += nb_time_can_rerun * nb_fuel_for_ore
+
+
 with open("data.txt") as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
@@ -126,17 +137,25 @@ run_system_until_only_ore_needed()
 # 1. We'll do as much fuel as we can with this single ore (just use a division to know how much, and multiply ALL
 # remaining chemicals,  and needed chemicals, by this result).
 min_ore_for_one_fuel = dict_needed_chemicals['ORE']
-nb_time_can_rerun = int(1000000000000 / min_ore_for_one_fuel)
-print(nb_time_can_rerun)
-
-for chemical_name, chemical_amount in dict_remaining_chemicals.items():
-    dict_remaining_chemicals[chemical_name] = nb_time_can_rerun * chemical_amount
-dict_needed_chemicals['ORE'] *= nb_time_can_rerun
+dict_chemical_producted = copy.deepcopy(dict_remaining_chemicals)
+produce_fuel_with_shortcut(1000000000000 - min_ore_for_one_fuel, min_ore_for_one_fuel)
 
 # 2. Then "revert" as many remaining chemicals as possible (except fuel, ofc). Repeat as long as you have
 # reversions you can do
 run_system_until_no_excess_product_remains(dict_reactions)
 
-print(dict_remaining_chemicals['FUEL'])
-print(dict_remaining_chemicals)
-print(dict_needed_chemicals)
+dict_needed_chemicals['ORE'] -= dict_remaining_chemicals['ORE']
+dict_remaining_chemicals['ORE'] = 0
+# 3. produce the fuel with the "ore per fuel" shortcut you already have for the diff between 1000000000000 and the
+# amount ore you already used, and repeat step 2 as long as necessary
+while 1000000000000 - dict_needed_chemicals['ORE'] >= min_ore_for_one_fuel:
+    produce_fuel_with_shortcut(1000000000000 - dict_needed_chemicals['ORE'], min_ore_for_one_fuel)
+    run_system_until_no_excess_product_remains(dict_reactions)
+    dict_needed_chemicals['ORE'] -= dict_remaining_chemicals['ORE']
+    dict_remaining_chemicals['ORE'] = 0
+
+
+while dict_needed_chemicals['ORE'] < 1000000000000:
+    produce_chemical('FUEL', 1, dict_reactions)
+    run_system_until_only_ore_needed()
+print(dict_remaining_chemicals['FUEL'] - 1)
