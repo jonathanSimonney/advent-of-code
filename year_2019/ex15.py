c4e7d8a19942120442cb69f0f2c09b34f5549f79
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-06-23 08:09:40 +0200

    part 1 of day15 complted

--- year_2019/ex15.py
+++ year_2019/ex15.py
@@ -19,7 +19,12 @@ class Direction(Enum):
 class Tiles(Enum):
     WALL = 0
     FLOOR = 1
-    UNKNOWN = 2
+    OXYGEN = 2
+    UNKNOWN = 3
+
+
+class AllAdjacentSpacesExploredException(Exception):
+    pass
 
 
 class Robot:
@@ -31,6 +36,10 @@ class Robot:
     y_pos = 0
     direction = Direction.NONE
     dict_known_pos = collections.defaultdict(lambda:  Tiles.UNKNOWN)
+    return_path = []
+    is_backtracking = False
+    targeted_x_pos = 0
+    targeted_y_pos = 0
 
     def __init__(self):
         self.dict_known_pos[compute_key_with_coords(self.x_pos, self.y_pos)] = Tiles.FLOOR
@@ -39,9 +48,16 @@ class Robot:
         # todo implement a move logic not relying on input. robot knows where he is and what spaces are around him,
         #  so he needs to either :  keep exploring RIGHT next to him, OR go to the closest unblocked space to explore
         self._print_known_space()
-        input_number = int(input('please write direction'))
-        self.direction = Direction(input_number)
-        return input_number
+
+        try:
+            self.is_backtracking = False
+            if self.dict_known_pos[compute_key_with_coords(self.targeted_x_pos, self.targeted_y_pos)] != Tiles.UNKNOWN:
+                self._compute_new_target_space()
+            self.direction = self._compute_direction_to_move_towards(self.targeted_x_pos, self.targeted_y_pos)
+        except AllAdjacentSpacesExploredException:
+            self.is_backtracking = True
+            self.direction = self.return_path.pop()
+        return self.direction.value
 
     def handle_output(self, output_value):
         post_move_x_pos = self.x_pos
@@ -69,15 +85,24 @@ class Robot:
             self.x_pos = post_move_x_pos
             self.y_pos = post_move_y_pos
             self.dict_known_pos[compute_key_with_coords(post_move_x_pos, post_move_y_pos)] = Tiles.FLOOR
+            if not self.is_backtracking:
+                self.return_path.append(get_opposite_direction(self.direction))
         elif output_value == 2:
             print(f"oxygen tank found, at pos : {post_move_x_pos}, {post_move_y_pos}")
+            # 203 too low
+            print(len(self.return_path))
+            # raise Exception
             self.x_pos = post_move_x_pos
             self.y_pos = post_move_y_pos
+            self.dict_known_pos[compute_key_with_coords(post_move_x_pos, post_move_y_pos)] = Tiles.OXYGEN
+            if not self.is_backtracking:
+                self.return_path.append(get_opposite_direction(self.direction))
 
     def _print_known_space(self):
         dict_symbols = {
             Tiles.UNKNOWN: ' ',
             Tiles.FLOOR: '.',
+            Tiles.OXYGEN: 'O',
             Tiles.WALL: '#',
         }
 
@@ -91,6 +116,47 @@ class Robot:
             str_to_print += '\n'
 
         print(str_to_print)
+
+    def _compute_new_target_space(self):
+        if self.dict_known_pos[compute_key_with_coords(self.x_pos + 1, self.y_pos)] == Tiles.UNKNOWN:
+            self._set_new_target_space(self.x_pos + 1, self.y_pos)
+        elif self.dict_known_pos[compute_key_with_coords(self.x_pos - 1, self.y_pos)] == Tiles.UNKNOWN:
+            self._set_new_target_space(self.x_pos - 1, self.y_pos)
+        elif self.dict_known_pos[compute_key_with_coords(self.x_pos, self.y_pos + 1)] == Tiles.UNKNOWN:
+            self._set_new_target_space(self.x_pos, self.y_pos + 1)
+        elif self.dict_known_pos[compute_key_with_coords(self.x_pos, self.y_pos - 1)] == Tiles.UNKNOWN:
+            self._set_new_target_space(self.x_pos, self.y_pos - 1)
+        else:
+            self._print_known_space()
+            raise AllAdjacentSpacesExploredException
+
+    def _compute_direction_to_move_towards(self, x, y):
+        if x == self.x_pos + 1 and y == self.y_pos:
+            return Direction.EAST
+        elif x == self.x_pos - 1 and y == self.y_pos:
+            return Direction.WEST
+        elif x == self.x_pos and y == self.y_pos + 1:
+            return Direction.NORTH
+        elif x == self.x_pos and y == self.y_pos - 1:
+            return Direction.SOUTH
+        else:
+            raise Exception
+
+    def _set_new_target_space(self, x, y):
+        self.targeted_x_pos = x
+        self.targeted_y_pos = y
+
+
+def get_opposite_direction(direction):
+    dict_opposed_directions = {
+        Direction.SOUTH: Direction.NORTH,
+        Direction.NORTH: Direction.SOUTH,
+        Direction.WEST: Direction.EAST,
+        Direction.EAST: Direction.WEST,
+    }
+
+    return dict_opposed_directions[direction]
+
 
 with open("data.txt") as f:
     content = f.readlines()
