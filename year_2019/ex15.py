7bb218cf0e7ffca9b4e3d0b2cc759766dbefe06d
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-06-28 08:07:32 +0200

    day 15 done properly

--- year_2019/ex15.py
+++ year_2019/ex15.py
@@ -4,10 +4,6 @@ from enum import Enum
 from year_2019.intcode_computer import IntcodeComputer
 
 
-def compute_key_with_coords(pos_x, pos_y):
-    return f"x{pos_x}y{pos_y}"
-
-
 class Direction(Enum):
     NONE = 0
     NORTH = 1
@@ -24,6 +20,10 @@ class Tiles(Enum):
 
 
 class AllAdjacentSpacesExploredException(Exception):
+    pass
+
+
+class IntcodeProgrammeFinished(Exception):
     pass
 
 
@@ -45,9 +45,7 @@ class Robot:
         self.dict_known_pos[compute_key_with_coords(self.x_pos, self.y_pos)] = Tiles.FLOOR
 
     def get_input(self):
-        # todo implement a move logic not relying on input. robot knows where he is and what spaces are around him,
-        #  so he needs to either :  keep exploring RIGHT next to him, OR go to the closest unblocked space to explore
-        self._print_known_space()
+        # self._print_known_space()
 
         try:
             self.is_backtracking = False
@@ -55,8 +53,11 @@ class Robot:
                 self._compute_new_target_space()
             self.direction = self._compute_direction_to_move_towards(self.targeted_x_pos, self.targeted_y_pos)
         except AllAdjacentSpacesExploredException:
-            self.is_backtracking = True
-            self.direction = self.return_path.pop()
+            if len(self.return_path) > 0:
+                self.is_backtracking = True
+                self.direction = self.return_path.pop()
+            else:
+                raise IntcodeProgrammeFinished
         return self.direction.value
 
     def handle_output(self, output_value):
@@ -98,7 +99,7 @@ class Robot:
             if not self.is_backtracking:
                 self.return_path.append(get_opposite_direction(self.direction))
 
-    def _print_known_space(self):
+    def compute_str_known_spaces(self):
         dict_symbols = {
             Tiles.UNKNOWN: ' ',
             Tiles.FLOOR: '.',
@@ -115,7 +116,10 @@ class Robot:
                     str_to_print += dict_symbols[self.dict_known_pos[compute_key_with_coords(x, y)]]
             str_to_print += '\n'
 
-        print(str_to_print)
+        return str_to_print
+
+    def _print_known_space(self):
+        print(self.compute_str_known_spaces())
 
     def _compute_new_target_space(self):
         if self.dict_known_pos[compute_key_with_coords(self.x_pos + 1, self.y_pos)] == Tiles.UNKNOWN:
@@ -127,7 +131,7 @@ class Robot:
         elif self.dict_known_pos[compute_key_with_coords(self.x_pos, self.y_pos - 1)] == Tiles.UNKNOWN:
             self._set_new_target_space(self.x_pos, self.y_pos - 1)
         else:
-            self._print_known_space()
+            # self._print_known_space()
             raise AllAdjacentSpacesExploredException
 
     def _compute_direction_to_move_towards(self, x, y):
@@ -145,6 +149,24 @@ class Robot:
     def _set_new_target_space(self, x, y):
         self.targeted_x_pos = x
         self.targeted_y_pos = y
+
+
+# todo do NOT copy paste this, but finally use https://docs.python.org/3/library/dataclasses.html.
+#  (eventually, also use explicit typing)
+def compute_key_with_coords(pos_x, pos_y):
+    return f"x{pos_x}y{pos_y}"
+
+
+def compute_adjacent_positions_list(pos_str_formatted):
+    pos_array = pos_str_formatted.split('y')
+    y = int(pos_array[1])
+    x = int(pos_array[0][1:])
+    return [
+        compute_key_with_coords(x + 1, y),
+        compute_key_with_coords(x - 1, y),
+        compute_key_with_coords(x, y + 1),
+        compute_key_with_coords(x, y - 1)
+    ]
 
 
 def get_opposite_direction(direction):
@@ -167,5 +189,36 @@ content = [int(x) for x in content[0].strip().split(",")]
 intcode_computer = IntcodeComputer(content)
 robot = Robot()
 
-intcode_computer.run_intcode_program_from_start(
-    get_input_instruction=robot.get_input, send_output_instruction=robot.handle_output)
+try:
+    intcode_computer.run_intcode_program_from_start(
+        get_input_instruction=robot.get_input, send_output_instruction=robot.handle_output)
+except IntcodeProgrammeFinished:
+    pass
+
+space_section = robot.compute_str_known_spaces()
+print(space_section)
+
+set_oxygen_spaces = set()
+set_floor_spaces = set()
+
+for pos_str, tile_type in robot.dict_known_pos.items():
+    if tile_type == Tiles.OXYGEN:
+        set_oxygen_spaces.add(pos_str)
+    elif tile_type == Tiles.FLOOR:
+        set_floor_spaces.add(pos_str)
+
+nb_tick_to_oxygen_whole_station = 0
+
+while len(set_floor_spaces) != 0:
+    nb_tick_to_oxygen_whole_station += 1
+    next_tick_set_oxygen_spaces = set()
+    for pos in set_oxygen_spaces:
+        list_adjacent_pos = compute_adjacent_positions_list(pos)
+        for adj_pos in list_adjacent_pos:
+            if adj_pos in set_floor_spaces:
+                next_tick_set_oxygen_spaces.add(adj_pos)
+                set_floor_spaces.remove(adj_pos)
+    set_oxygen_spaces = next_tick_set_oxygen_spaces
+
+# 340 right answer !
+print(nb_tick_to_oxygen_whole_station)
