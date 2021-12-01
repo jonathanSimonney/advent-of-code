7388ac10a132966ade6d0af1af4cf95440085bd3
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-12-01 07:53:11 +0100

    day 1 completed

---
+++ year_2021/ex1.py
@@ -0,0 +1,13 @@
+with open("data.txt") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [int(x.strip()) for x in content]
+
+old_value = sum(content[:3])
+acc = 0
+for index in range(len(content) - 3):
+    if content[index + 3] > content[index]:
+        acc += 1
+
+# 1682 too low
+print(acc)
