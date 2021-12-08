2f7a6e42db5f6a2691ccbbfc37a06827eb654f65
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-12-08 09:13:48 +0100

    ex7 part 1 done

--- year_2021/ex7.py
+++ year_2021/ex7.py
@@ -13,12 +13,19 @@ print(dict_content)
 dict_alignment_cost: dict = {}
 
 previous_iteration_key = None
+previous_pos_sum = 0
+future_pos_sum = sum(dict_content.values())
 for key in dict_content.keys():
     if key == 0:
         dict_alignment_cost[key] = sum(content)
     else:
-        # find the alignment costr for next value thanks to previous value
-        dict_alignment_cost[key] = dict_alignment_cost[previous_iteration_key] + (dict_content[previous_iteration_key] - )
+        # find the alignment cost for next value thanks to previous value
+        dict_alignment_cost[key] = \
+            dict_alignment_cost[previous_iteration_key] + \
+            (key - previous_iteration_key) * (previous_pos_sum - future_pos_sum)
     previous_iteration_key = key
+    previous_pos_sum += dict_content[key]
+    future_pos_sum -= dict_content[key]
 
+print(dict_alignment_cost)
 print(min(dict_alignment_cost.values()))
