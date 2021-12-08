32dbbeabdaf930965c9011dbf08808d8fe5b2aa1
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-12-08 09:41:02 +0100

    ex7 part 2 done

--- year_2021/ex7.py
+++ year_2021/ex7.py
@@ -1,5 +1,25 @@
 import collections
 
+
+dict_alignment_cost_per_distance = {0: 0}
+
+
+def compute_cost_move_of_distance(move_length: int) -> int:
+    if move_length not in dict_alignment_cost_per_distance:
+        dict_alignment_cost_per_distance[move_length] = compute_cost_move_of_distance(move_length - 1) + move_length
+    return dict_alignment_cost_per_distance[move_length]
+
+
+def compute_cost_alignment_on_pos(dict_nb_crabs_on_pos: dict, pos_tested: int) -> int:
+    total_cost = 0
+    for init_pos, nb_crabs_on_pos in dict_nb_crabs_on_pos.items():
+        key_alignment_cost = abs(pos_tested - init_pos)
+        if key_alignment_cost not in dict_alignment_cost_per_distance:
+            dict_alignment_cost_per_distance[key_alignment_cost] = compute_cost_move_of_distance(key_alignment_cost)
+        total_cost += nb_crabs_on_pos * dict_alignment_cost_per_distance[key_alignment_cost]
+    return total_cost
+
+
 with open("data.txt") as f:
     content = f.readlines()[0].split(',')
 # you may also want to remove whitespace characters like `\n` at the end of each line
@@ -15,17 +35,7 @@ dict_alignment_cost: dict = {}
 previous_iteration_key = None
 previous_pos_sum = 0
 future_pos_sum = sum(dict_content.values())
-for key in dict_content.keys():
-    if key == 0:
-        dict_alignment_cost[key] = sum(content)
-    else:
-        # find the alignment cost for next value thanks to previous value
-        dict_alignment_cost[key] = \
-            dict_alignment_cost[previous_iteration_key] + \
-            (key - previous_iteration_key) * (previous_pos_sum - future_pos_sum)
-    previous_iteration_key = key
-    previous_pos_sum += dict_content[key]
-    future_pos_sum -= dict_content[key]
+for key in range(max(dict_content.keys())):
+    dict_alignment_cost[key] = compute_cost_alignment_on_pos(dict_content, key)
 
-print(dict_alignment_cost)
 print(min(dict_alignment_cost.values()))
