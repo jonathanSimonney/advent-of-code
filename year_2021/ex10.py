ed0fa3a642525e99ebaa8e57c41e8a8e7e2cb488
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-12-10 08:29:51 +0100

    ex10 part 2 done

--- year_2021/ex10.py
+++ year_2021/ex10.py
@@ -1,7 +1,19 @@
+from statistics import median
+
 dict_opening_to_closing_char = {'(': ')', '{': '}', '<': '>', '[': ']'}
 
 
-def find_first_invalid_char_closing(line_param: str) -> str:
+def compute_line_score(line_to_compute) -> int:
+    dict_completion_char_score = {')': 1, '}': 3, '>': 4, ']': 2}
+
+    acc = 0
+    for char in line_to_compute:
+        acc *= 5
+        acc += dict_completion_char_score[char]
+    return acc
+
+
+def find_first_invalid_char_closing_or_needed_completion_line(line_param: str) -> str:
     list_closing_char_expected: list[str] = []
     for char in line_param:
         if char in dict_opening_to_closing_char.keys():
@@ -11,7 +23,8 @@ def find_first_invalid_char_closing(line_param: str) -> str:
             if char != expected_char:
                 return char
 
-    return 'valid_line'
+    list_closing_char_expected.reverse()
+    return ''.join(list_closing_char_expected)
 
 
 with open("data.txt") as f:
@@ -19,14 +32,22 @@ with open("data.txt") as f:
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [line.strip() for line in content]
 
-dict_invalid_char_num = {')': 0, '}': 0, '>': 0, ']': 0, 'valid_line': 0}
+dict_invalid_char_num = {')': 0, '}': 0, '>': 0, ']': 0}
+list_completion_line_score: list[int] = []
 
 for line in content:
-    invalid_char = find_first_invalid_char_closing(line)
-    dict_invalid_char_num[invalid_char] += 1
+    invalid_char_or_completion_line = find_first_invalid_char_closing_or_needed_completion_line(line)
+    if invalid_char_or_completion_line in dict_invalid_char_num.keys():
+        dict_invalid_char_num[invalid_char_or_completion_line] += 1
+    else:
+        list_completion_line_score.append(compute_line_score(invalid_char_or_completion_line))
 
 print(3 * dict_invalid_char_num[')'] +
       57 * dict_invalid_char_num[']'] +
       1197 * dict_invalid_char_num['}'] +
       25137 * dict_invalid_char_num['>']
       )
+
+# 4144007457 too high
+list_completion_line_score.sort()
+print(median(list_completion_line_score))
