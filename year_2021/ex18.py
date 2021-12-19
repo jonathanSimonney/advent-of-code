ab55c1dbd299be8317cea42dd58635cfb8fc7902
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2021-12-19 21:42:03 +0100

    reduce now fully working, hopefully

--- year_2021/ex18.py
+++ year_2021/ex18.py
@@ -1,3 +1,4 @@
+import math
 from statistics import median
 from typing import Union
 
@@ -83,6 +84,7 @@ def reduce_snailfish_str(str_to_reduce: str) -> str:
     must_explode_next_snailfish_num = False
     snailfish_num_to_explode = ''
     nb_opened_brackets = 0
+    # first, we EXPLODE
     for char in str_to_reduce:
         if not must_explode_next_snailfish_num:
             str_list_already_seen.append(char)
@@ -95,7 +97,6 @@ def reduce_snailfish_str(str_to_reduce: str) -> str:
         elif char == ']':
             nb_opened_brackets -= 1
             if must_explode_next_snailfish_num:
-                must_explode_next_snailfish_num = False
                 snailfish_num_as_list = snailfish_num_to_explode.split(',')
                 snailfish_num_left = int(snailfish_num_as_list[0])
                 snailfish_num_right = int(snailfish_num_as_list[1])
@@ -106,8 +107,32 @@ def reduce_snailfish_str(str_to_reduce: str) -> str:
             if must_explode_next_snailfish_num:
                 snailfish_num_to_explode += char
 
-    return str_to_reduce
+    # then, we SPLIT
+    str_list_not_yet_seen = list(str_to_reduce)
+    str_list_already_seen = []
+    str_num = ''
+    for char in str_to_reduce:
+        str_list_already_seen.append(char)
+        if char in ['[', ']', ',']:
+            if str_num != '':
+                str_list_already_seen.pop()
+                if int(str_num) >= 10:
+                    halved_num = int(str_num) / 2
+                    replacement_snailfish_num = f"[{str(math.floor(halved_num))},{str(math.ceil(halved_num))}]"
 
+                    new_str = ''.join(str_list_already_seen) + replacement_snailfish_num + ''.join(str_list_not_yet_seen)
+
+                    return reduce_snailfish_str(new_str)
+
+                str_list_already_seen.append(str_num)
+                str_list_already_seen.append(char)
+                str_num = ''
+        else:
+            str_num += char
+            str_list_already_seen.pop()
+        str_list_not_yet_seen.pop(0)
+
+    return str_to_reduce
 
 
 with open("data.txt") as f:
@@ -115,4 +140,4 @@ with open("data.txt") as f:
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [line.strip() for line in content]
 
-print(reduce_snailfish_str('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]'))
+print(reduce_snailfish_str('[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]'))
