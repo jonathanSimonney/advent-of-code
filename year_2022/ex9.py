c30d845422d2af85127cb2737fd3290ef165b050
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2022-12-11 23:50:49 +0100

    part 2 done

--- year_2022/ex9.py
+++ year_2022/ex9.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Set
+from typing import Set, List
 
 from common_helpers.position import Direction, Position
 
@@ -23,37 +23,40 @@ def parse_line_instruction(line_parsed: str) -> Movement:
 
 class System:
     head_rope_pos: Position
-    tail_rope_pos: Position
+    list_rope_knots: List[Position]
     set_pos_visited_by_tail: Set[Position]
 
     def __init__(self):
-        self.head_rope_pos = Position(0, 0)
-        self.tail_rope_pos = Position(0, 0)
-        self.set_pos_visited_by_tail = {self.head_rope_pos}
+        self.list_rope_knots = [Position(0, 0)] * 10
+        self.set_pos_visited_by_tail = {Position(0, 0)}
 
     def move_head_in_direction(self, direction_movement: Direction):
-        self.head_rope_pos = self.head_rope_pos.get_pos_direction(direction_movement)
-        if self.tail_rope_pos == self.head_rope_pos:
-            pass
-        elif self.tail_rope_pos.get_pos_direction_and_space(Direction.UP, 2) == self.head_rope_pos:
-            self.tail_rope_pos = self.tail_rope_pos.get_pos_direction(Direction.UP)
-        elif self.tail_rope_pos.get_pos_direction_and_space(Direction.DOWN, 2) == self.head_rope_pos:
-            self.tail_rope_pos = self.tail_rope_pos.get_pos_direction(Direction.DOWN)
-        elif self.tail_rope_pos.get_pos_direction_and_space(Direction.LEFT, 2) == self.head_rope_pos:
-            self.tail_rope_pos = self.tail_rope_pos.get_pos_direction(Direction.LEFT)
-        elif self.tail_rope_pos.get_pos_direction_and_space(Direction.RIGHT, 2) == self.head_rope_pos:
-            self.tail_rope_pos = self.tail_rope_pos.get_pos_direction(Direction.RIGHT)
-        elif self.head_rope_pos not in self.tail_rope_pos.get_all_adjacent_pos():
-            possible_pos = set(self.head_rope_pos.get_all_adjacent_pos()).intersection(
-                self.tail_rope_pos.get_all_diagonal_pos()
+        self.list_rope_knots[0] = self.list_rope_knots[0].get_pos_direction(direction_movement)
+        for idx, knot in enumerate(self.list_rope_knots[1:]):
+            self.list_rope_knots[idx + 1] = self.move_knot_following_position(self.list_rope_knots[idx], knot)
+        self.set_pos_visited_by_tail.add(self.list_rope_knots[-1])
+
+    @staticmethod
+    def move_knot_following_position(knot_pos_to_follow: Position, knot_following_pos: Position) -> Position:
+        if knot_following_pos == knot_pos_to_follow:
+            print("first if")
+            return knot_following_pos
+        elif knot_following_pos.get_pos_direction_and_space(Direction.UP, 2) == knot_pos_to_follow:
+            return knot_following_pos.get_pos_direction(Direction.UP)
+        elif knot_following_pos.get_pos_direction_and_space(Direction.DOWN, 2) == knot_pos_to_follow:
+            return knot_following_pos.get_pos_direction(Direction.DOWN)
+        elif knot_following_pos.get_pos_direction_and_space(Direction.LEFT, 2) == knot_pos_to_follow:
+            return knot_following_pos.get_pos_direction(Direction.LEFT)
+        elif knot_following_pos.get_pos_direction_and_space(Direction.RIGHT, 2) == knot_pos_to_follow:
+            return knot_following_pos.get_pos_direction(Direction.RIGHT)
+        elif knot_pos_to_follow not in knot_following_pos.get_all_adjacent_pos():
+            possible_pos = set(knot_pos_to_follow.get_all_adjacent_pos()).intersection(
+                knot_following_pos.get_all_diagonal_pos()
             )
             if len(possible_pos) != 1:
-                print(possible_pos)
-                print(self.head_rope_pos, self.tail_rope_pos)
                 raise AssertionError("thought diagonal possible moves were LIMITED to one")
-            self.tail_rope_pos = possible_pos.pop()
-
-        self.set_pos_visited_by_tail.add(self.tail_rope_pos)
+            return possible_pos.pop()
+        return knot_following_pos
 
 
 def main():
